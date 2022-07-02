a88ac4626d0d63e424e0119b44ba54061de6eb90
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-07-02 18:58:16 -0300

    fix: return likes after like

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(
+  const repositoryIndex = repositories.findindex(
     (repository) => repository.id === id
   );
 
@@ -50,7 +50,7 @@ app.put("/repositories/:id", (request, response) => {
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(
+  const repositoryIndex = repositories.findIndex(
     (repository) => repository.id === id
   );
 
@@ -66,7 +66,7 @@ app.delete("/repositories/:id", (request, response) => {
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(
+  const repositoryIndex = repositories.findIndex(
     (repository) => repository.id === id
   );
 
@@ -76,7 +76,9 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json("likes");
+  return response.json({
+    likes,
+  });
 });
 
 module.exports = app;
